0cda143a48bc2a0253e77ae884a924faf17e38fc
qumengdie <qu_mengdie@126.com>
2025-09-24 16:20:53 -0400

    Category Module

---
+++ backend/src/main/java/com/qumengdie/ecommerce/service/CategoryService.java
@@ -0,0 +1,14 @@
+package com.qumengdie.ecommerce.service;
+
+import com.qumengdie.ecommerce.model.Category;
+import java.util.List;
+
+public interface CategoryService {
+  List<Category> getAllCategories();
+
+  void createCategory(Category category);
+
+  String deleteCategory(Long categoryId);
+
+  Category updateCategory(Category category, Long categoryId);
+}
---
+++ backend/src/main/java/com/qumengdie/ecommerce/service/CategoryServiceImpl.java
@@ -0,0 +1,52 @@
+package com.qumengdie.ecommerce.service;
+
+import com.qumengdie.ecommerce.model.Category;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+  private List<Category> categories = new ArrayList<>();
+  private Long nextId = 1L;
+
+  @Override
+  public List<Category> getAllCategories() {
+    return categories;
+  }
+
+  @Override
+  public void createCategory(Category category) {
+    category.setCategoryId(nextId++);
+    categories.add(category);
+  }
+
+  @Override
+  public String deleteCategory(Long categoryId) {
+    Category category =
+        categories.stream()
+            .filter(c -> c.getCategoryId().equals(categoryId))
+            .findFirst()
+            .orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found"));
+
+    categories.remove(category);
+    return "Category with Id " + categoryId + " removed";
+  }
+
+  @Override
+  public Category updateCategory(Category category, Long categoryId) {
+    Category existingCategory =
+        categories.stream()
+            .filter(c -> c.getCategoryId().equals(categoryId))
+            .findFirst()
+            .orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found"));
+
+    existingCategory.setCategoryName(category.getCategoryName());
+    return existingCategory;
+  }
+}
