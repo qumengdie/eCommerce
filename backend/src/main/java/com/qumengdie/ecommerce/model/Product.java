ca98a816dd763d8458063a926d1c337dc7290ae4
qumengdie <qu_mengdie@126.com>
2025-09-29 16:36:56 -0400

    User, address, roles

--- backend/src/main/java/com/qumengdie/ecommerce/model/Product.java
+++ backend/src/main/java/com/qumengdie/ecommerce/model/Product.java
@@ -5,11 +5,14 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "products")
+@ToString
 public class Product {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
@@ -26,4 +29,8 @@ public class Product {
   @ManyToOne
   @JoinColumn(name = "category_id")
   private Category category;
+
+  @ManyToOne
+  @JoinColumn(name = "seller_id")
+  private User user;
 }
