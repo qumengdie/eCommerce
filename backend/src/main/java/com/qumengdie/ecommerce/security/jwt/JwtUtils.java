59ed39bc29e788ba1b0556c26c5e0629371315a6
qumengdie <qu_mengdie@126.com>
2025-09-30 15:42:23 -0400

    get user, sign out

--- backend/src/main/java/com/qumengdie/ecommerce/security/jwt/JwtUtils.java
+++ backend/src/main/java/com/qumengdie/ecommerce/security/jwt/JwtUtils.java
@@ -60,6 +60,10 @@ public class JwtUtils {
         .build();
   }
 
+  public ResponseCookie getCleanJwtCookie() {
+    return ResponseCookie.from(jwtCookie, null).path("/api").build();
+  }
+
   public String generateTokenFromUsername(String username) {
     return Jwts.builder()
         .subject(username)
