a7ddbfcd62357f065de54fe69571ea8632fed25d
qumengdie <qu_mengdie@126.com>
2025-10-08 13:52:18 -0400

    fetch categories

--- frontend/ecom-frontend/src/components/Filter.jsx
+++ frontend/ecom-frontend/src/components/Filter.jsx
@@ -10,15 +10,7 @@ import { useEffect, useState } from 'react';
 import { FiArrowDown, FiArrowUp, FiRefreshCw, FiSearch } from 'react-icons/fi';
 import { useLocation, useNavigate, useSearchParams } from 'react-router-dom';
 
-const Filter = () => {
-  const categories = [
-    { categoryId: 1, categoryName: 'Sports' },
-    { categoryId: 2, categoryName: 'Laptop' },
-    { categoryId: 3, categoryName: 'Furniture' },
-    { categoryId: 4, categoryName: 'Books' },
-    { categoryId: 5, categoryName: 'Toys' },
-  ];
-
+const Filter = ({ categories }) => {
   const [searchParams] = useSearchParams();
   const params = new URLSearchParams(searchParams);
   const pathname = useLocation().pathname;
