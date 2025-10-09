c31204459fd6636aefae472baaf6148ae025e4e1
qumengdie <qu_mengdie@126.com>
2025-10-09 11:04:13 -0400

    navbar

--- frontend/ecom-frontend/src/App.jsx
+++ frontend/ecom-frontend/src/App.jsx
@@ -2,10 +2,12 @@ import './App.css';
 import Home from './components/home/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Products from './components/products/Products';
+import Navbar from './components/shared/Navbar';
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
