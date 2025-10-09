4d61cdcab11891f27863607d2f6704409cff3f03
qumengdie <qu_mengdie@126.com>
2025-10-09 17:31:03 -0400

    cart page

--- frontend/ecom-frontend/src/App.jsx
+++ frontend/ecom-frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Products from './components/products/Products';
 import Navbar from './components/shared/Navbar';
 import About from './components/About';
 import Contact from './components/Contact';
+import Cart from './components/cart/Cart';
 import { Toaster } from 'react-hot-toast';
 import React from 'react';
 
@@ -18,6 +19,7 @@ function App() {
           <Route path="/products" element={<Products />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/cart" element={<Cart />} />
         </Routes>
       </Router>
       <Toaster position="bottom-center" />
