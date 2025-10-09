749869a329badaf62ffb2068d36c30002743193f
qumengdie <qu_mengdie@126.com>
2025-10-08 23:22:48 -0400

    Routing, home page banners

--- frontend/ecom-frontend/src/App.jsx
+++ frontend/ecom-frontend/src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css';
+import Home from './components/home/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Products from './components/products/Products';
 
 function App() {
   return (
-    <>
-      <Products />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
+    </Router>
   );
 }
 
--- frontend/ecom-frontend/src/main.jsx
+++ frontend/ecom-frontend/src/main.jsx
@@ -9,9 +9,7 @@ import { BrowserRouter } from 'react-router-dom';
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </StrictMode>
   </Provider>
 );
