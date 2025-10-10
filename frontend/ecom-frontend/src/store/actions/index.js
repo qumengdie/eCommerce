58373967859bb7183a37f14cc67ef7e44671c9fe
qumengdie <qu_mengdie@126.com>
2025-10-10 16:51:27 -0400

    log out functionality

--- frontend/ecom-frontend/src/store/actions/index.js
+++ frontend/ecom-frontend/src/store/actions/index.js
@@ -149,3 +149,9 @@ export const registerNewUser =
       setLoader(false);
     }
   };
+
+export const logOutUser = (navigate) => (dispatch) => {
+  dispatch({ type: 'LOG_OUT' });
+  localStorage.removeItem('auth');
+  navigate('/login');
+};
