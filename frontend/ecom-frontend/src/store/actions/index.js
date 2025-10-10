32a56946feaaf79eb0d79a59858f85a26cb7d279
qumengdie <qu_mengdie@126.com>
2025-10-10 16:46:18 -0400

    register functionality

--- frontend/ecom-frontend/src/store/actions/index.js
+++ frontend/ecom-frontend/src/store/actions/index.js
@@ -129,3 +129,23 @@ export const authenticateSingInUser =
       setLoader(false);
     }
   };
+
+export const registerNewUser =
+  (sendData, toast, reset, navigate, setLoader) => async (dispatch) => {
+    try {
+      setLoader(true);
+      const { data } = await api.post('/auth/signup', sendData);
+      reset();
+      toast.success(data?.message || 'User Registered Successfully');
+      navigate('/login');
+    } catch (error) {
+      console.log(error);
+      toast.error(
+        error?.response?.data?.message ||
+          error?.response?.data?.password ||
+          'Internal Server Error'
+      );
+    } finally {
+      setLoader(false);
+    }
+  };
