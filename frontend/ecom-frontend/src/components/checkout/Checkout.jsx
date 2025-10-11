eedb07ad2c243a30b7e459553677bafed3485e12
qumengdie <qu_mengdie@126.com>
2025-10-11 16:38:33 -0400

    AddressInfo page layout

--- frontend/ecom-frontend/src/components/checkout/Checkout.jsx
+++ frontend/ecom-frontend/src/components/checkout/Checkout.jsx
@@ -1,5 +1,6 @@
 import { Step, StepLabel, Stepper } from '@mui/material';
 import React, { useState } from 'react';
+import AddressInfo from './AddressInfo';
 
 const Checkout = () => {
   const [activeStep, setActiveStep] = useState(0);
@@ -15,6 +16,8 @@ const Checkout = () => {
           </Step>
         ))}
       </Stepper>
+
+      <div className="mt-5">{activeStep === 0 && <AddressInfo />}</div>
     </div>
   );
 };
