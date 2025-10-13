1d7160740eaeb484f8b2de2adb9f8e61fe3864cf
qumengdie <qu_mengdie@126.com>
2025-10-13 15:41:26 -0400

    send cart to backend from frontend

--- frontend/ecom-frontend/src/main.jsx
+++ frontend/ecom-frontend/src/main.jsx
@@ -8,8 +8,6 @@ import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <StrictMode>
-      <App />
-    </StrictMode>
+    <App />
   </Provider>
 );
