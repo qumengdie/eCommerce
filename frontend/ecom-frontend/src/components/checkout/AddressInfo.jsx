e0364b9db023f97960917ef28d574fc6d8094ec6
qumengdie <qu_mengdie@126.com>
2025-10-13 11:33:01 -0400

    display address list

--- frontend/ecom-frontend/src/components/checkout/AddressInfo.jsx
+++ frontend/ecom-frontend/src/components/checkout/AddressInfo.jsx
@@ -3,8 +3,10 @@ import React, { useState } from 'react';
 import { FaAddressBook } from 'react-icons/fa';
 import AddressInfoModal from './AddressInfoModal';
 import AddAddressForm from './AddAddressForm';
+import { useSelector } from 'react-redux';
+import AddressList from './AddressList';
 
-const AddressInfo = () => {
+const AddressInfo = ({ address }) => {
   const [openAddressModal, setOpenAddressModal] = useState(false);
   const [selectedAddress, setSelectedAddress] = useState('');
   const addNewAddressHandler = () => {
@@ -12,8 +14,8 @@ const AddressInfo = () => {
     setOpenAddressModal(true);
   };
 
-  const noAddressExist = true;
-  const isLoading = false;
+  const noAddressExist = !address || address.length === 0;
+  const { isLoading, btnLoader } = useSelector((state) => state.errors);
   return (
     <div className="pt-4">
       {noAddressExist ? (
@@ -44,9 +46,27 @@ const AddressInfo = () => {
               <Skeleton />
             </div>
           ) : (
-            <div className="space-y-4 pt-6">
-              <p>Address list here...</p>
-            </div>
+            <>
+              <div className="space-y-4 pt-6">
+                <p>Address list here...</p>
+                <AddressList
+                  addresses={address}
+                  setSelectedAddress={setSelectedAddress}
+                  setOpenAddressModal={setOpenAddressModal}
+                />
+              </div>
+
+              {address.length > 0 && (
+                <div className="mt-4">
+                  <button
+                    onClick={addNewAddressHandler}
+                    className="px-4 py-2 bg-blue-600 text-white font-medium rounded hover:bg-blue-700 transition-all"
+                  >
+                    Add More
+                  </button>
+                </div>
+              )}
+            </>
           )}
         </div>
       )}
