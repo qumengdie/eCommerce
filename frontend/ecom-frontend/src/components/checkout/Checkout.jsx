59c362d99214659c9656cb34f7b7a124f79383f3
qumengdie <qu_mengdie@126.com>
2025-10-13 14:31:43 -0400

    Error page

--- frontend/ecom-frontend/src/components/checkout/Checkout.jsx
+++ frontend/ecom-frontend/src/components/checkout/Checkout.jsx
@@ -4,6 +4,7 @@ import AddressInfo from './AddressInfo';
 import { useDispatch, useSelector } from 'react-redux';
 import { getUserAddresses } from '../../store/actions';
 import toast from 'react-hot-toast';
+import ErrorPage from '../shared/ErrorPage';
 
 const Checkout = () => {
   const [activeStep, setActiveStep] = useState(0);
@@ -95,6 +96,8 @@ const Checkout = () => {
           </button>
         )}
       </div>
+
+      {errorMessage && <ErrorPage message={errorMessage} />}
     </div>
   );
 };
