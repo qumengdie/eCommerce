3a11f9c51db466f38697e086497aa3b09859fba3
qumengdie <qu_mengdie@126.com>
2025-10-16 17:11:40 -0400

    dashboard page overview display

--- frontend/ecom-frontend/src/components/admin/dashboard/Dashboard.jsx
+++ frontend/ecom-frontend/src/components/admin/dashboard/Dashboard.jsx
@@ -1,7 +1,43 @@
 import React from 'react';
+import DashboardOverview from './DashboardOverview';
+import { FaBoxOpen, FaDollarSign, FaShoppingCart } from 'react-icons/fa';
+import { MdAttachMoney } from 'react-icons/md';
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const { productCount, totalRevenue, totalOrders } = {
+    productCount: '4',
+    totalRevenue: '999.0',
+    totalOrders: '5',
+  };
+
+  return (
+    <div>
+      <div
+        className="flex md:flex-row mt-8 flex-col lg:justify-between 
+          border border-slate-400 rounded-lg bg-linear-to-r
+           from-blue-50 to-blue-100 shadow-lg"
+      >
+        <DashboardOverview
+          title="Total Products"
+          amount={productCount}
+          Icon={FaBoxOpen}
+        />
+
+        <DashboardOverview
+          title="Total Orders"
+          amount={totalOrders}
+          Icon={FaShoppingCart}
+        />
+
+        <DashboardOverview
+          title="Total Revenue"
+          amount={totalRevenue}
+          Icon={FaDollarSign}
+          revenue
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
