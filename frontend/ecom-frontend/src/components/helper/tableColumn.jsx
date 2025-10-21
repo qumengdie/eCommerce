2506964817fddfe93b5d15f9faf00fc4dd3623c3
qumengdie <qu_mengdie@126.com>
2025-10-21 16:11:22 -0400

    admin order edit layout

--- frontend/ecom-frontend/src/components/helper/tableColumn.jsx
+++ frontend/ecom-frontend/src/components/helper/tableColumn.jsx
@@ -1,6 +1,6 @@
 import { FaEdit } from 'react-icons/fa';
 
-export const adminOrderTableColumn = [
+export const adminOrderTableColumn = (handleEdit) => [
   {
     sortable: false,
     disableColumnMenu: true,
@@ -83,7 +83,10 @@ export const adminOrderTableColumn = [
     renderCell: (params) => {
       return (
         <div className="flex justify-center items-center space-x-2 h-full pt-2">
-          <button className="flex items-center bg-blue-500 text-white px-4 h-9 rounded-md">
+          <button
+            onClick={() => handleEdit(params.row)}
+            className="flex items-center bg-blue-500 text-white px-4 h-9 rounded-md"
+          >
             <FaEdit className="mr-2" />
             Edit
           </button>
